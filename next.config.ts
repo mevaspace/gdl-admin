771419a7d1b75351b416547a0873584fa7d50bdf
haloivanid <ivanazis.id@gmail.com>
2026-05-18 02:21:36 +0800

    ci: failed run puppeteer

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.18.94'],
+  serverExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
 };
 
 export default nextConfig;
